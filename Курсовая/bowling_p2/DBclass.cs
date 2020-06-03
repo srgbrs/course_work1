27c425a64ac142dad33b046b92e56b1a62938d08
srgbrs <borisov11092000@gmail.com>
2020-06-03 23:15:06 +0300

    DataBase class added

---
+++ Курсовая/bowling_p2/DBclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+namespace bowling_p2
+{
+    public class DBclass
+    {
+        public DBclass()
+        {
+        }
+
+    
+    }
+}
