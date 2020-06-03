c432bebca6befa855c65bff99742c42c8d3e7305
srgbrs <borisov11092000@gmail.com>
2020-06-03 22:37:54 +0300

    Frame class added

---
+++ Курсовая/bowling_p2/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace bowling_p2
+{
+    public class Player
+    {
+        private string name;
+        private int resultScore;
+
+        public void SetName(string _name)
+        {
+            this.name = _name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetResult(int _result)
+        {
+            this.resultScore = _result;
+        }
+    }
+}
