045e2c6555fac62c22609eb75d591ba567d294a6
srgbrs <borisov11092000@gmail.com>
2020-06-03 22:27:33 +0300

    First Commit

---
+++ Курсовая/bowling_p2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bowling_p2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameMenu menu = new GameMenu();
+            menu.Start();
+        }
+    }
+}
